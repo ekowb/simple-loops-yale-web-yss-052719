a287c9401d3d9cda81e217b1c434cbc72783bbd4
Ekow Buadu <ekow.buadu@yale.edu>
2019-05-21 01:35:42 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -60,5 +60,6 @@ def for_iterator(number_of_times)
   # remember that `for` requires a range of numbers. How can we turn our number into a range?
 
   phrase = "Welcome to Flatiron School's Web Development Course!"
+  times = 1...number_of_times
 
 end
